0932e6df3f42123b238d0e28596d31484b8e8f99
Elvin Baxsh <47717988+elvinbaxsh28@users.noreply.github.com>
2022-11-11 18:21:16 +0400

    commits Test

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './testimonials.css';
-import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR1 from '../../assets/avatar1.jpg'
 import AVTR2 from '../../assets/avatar2.jpg';
 import AVTR3 from '../../assets/avatar3.jpg';
 import AVTR4 from '../../assets/avatar4.jpg';
 
+import { Pagination} from 'swiper';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+//import 'swiper/css/navigation';
+
+//import 'swiper/css/scrollbar';
 
 
 
@@ -13,55 +21,76 @@ const data = [
   {
     avatar: AVTR1,
     name:'Tina Snow',
-    rewiev: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur.'
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
   },
   {
     avatar: AVTR2,
     name:'Tina Snow',
-    rewiev: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur.'
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur.'
   },
     {avatar: AVTR3,
     name:'Tina Snow',
-    rewiev: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur.'
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur.'
     },
     {
       avatar: AVTR4,
     name:'Tina Snow',
-    rewiev: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur.'
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis praesentium mollitia quasi modi unde possimus delectus a dolores expedita consequuntur.'
     }
   ]
 
 const Testimonials = () => {
   return (
-    //<section id='testimonials'>Testimonials</section>
+    
     <section id='testimonials'>
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials_container">
+      <Swiper className="container testimonials_container"
+       modules={[Pagination]}
+       spaceBetween={40}
+       slidesPerView={1}
+       pagination={{ clickable: true }}
+    
+      >
        {
-        data.map(({image , avatar, name , rewiev}, index) => {
+        data.map(({avatar , name , review}, index) => {
 
         return(
-          <article key={index} className='testimonials'>
+          <SwiperSlide key={index} className='testimonials'>
           <div className="client_avatar">
-            <img src={image} alt="" />
+            <img src={avatar} alt="" />
             </div>
           <div>
           <h5 className='clients_name'>{name}</h5>
             <small className='clients_review'>
-               {rewiev}
+               {review}
             </small>
           </div>
 
-        </article>
+        </SwiperSlide>
         )
         })
        }
-       </div>
+       </Swiper>
 
     </section>
 
   )
 }
 export default Testimonials
+
+
+
+
+// {/* <section id='testimonials'>Testimonials
+//     <h5>Review from clients</h5>
+
+//     <div className="container testimonials_container">
+//         <article className="testimonial">
+//         <div className="client_avatar">
+//             <img src={AVTR1} alt="" />
+//             </div>
+//         </article>
+//     </div>
+//     </section> */}
